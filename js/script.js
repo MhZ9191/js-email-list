1693ae2139254cc62ddb0b69fa493a1cef5bd7cc
MhZ9191 <hegic91@gmail.com>
2026-01-22 13:53:45 +0100

    Added first little step

---
+++ js/script.js
@@ -0,0 +1,30 @@
+/*
+Descrizione
+Attraverso l'apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
+Bonus
+
+    Abbellire con CSS o Bootstrap
+    Inserire un bottone che al click faccia il fetch altre 10 mail 
+    (sostituendo le altre)
+    Buon Lavoro
+*/
+
+const subContainer = document.getElementById("subcontainer");
+const btn = document.querySelector(".btn");
+const test = document.createElement("div");
+
+btn.addEventListener("click", generate);
+
+function generate() {
+  for (let i = 0; i < 10; i++) {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/random/mail")
+      .then((res) => {
+        const mail = res.data.response;
+        console.log(mail);
+      });
+  }
+}
